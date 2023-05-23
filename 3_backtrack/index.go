2ede46027d5c05304d9fedffba63710fc4590449
qiaochao <qiaochao@icourt.cc>
2023-05-23 16:17:41 +0800

    feat : Fibonacci

--- 3_backtrack/index.go
+++ 3_backtrack/index.go
@@ -1,8 +1,1 @@
 package backtrack
-
-import "fmt"
-
-// 这是因为 Go 只允许 package main 下的代码直接运行,其他包需要先生成可执行文件。
-func main() {
-	fmt.Println("hello world")
-}
--- index.go
+++ index.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	dp "qc-go-algorithm/1_dp"
+)
 
+func main() {
+	testDp()
+}
+
+func testDp() {
+	//1.斐波那契数
+	result := dp.Fibonacci(9)
+	fmt.Println(result)
 }
