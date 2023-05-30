136716baf1505eef4e6cb69c77266ee4c62f21e1
qiaochao <qiaochao@icourt.cc>
2023-05-30 15:09:22 +0800

    feat : btree

--- 5_btree/2_二叉树的最小深度.go
+++ 5_btree/2_二叉树的最小深度.go
@@ -7,6 +7,38 @@ import "qc-go-algorithm/lib"
 func MinDepth(root *lib.TreeNode) int {
 
 	//先考虑边界情况
+	//如果根节点为空，返回0
+	if root == nil {
+		return 0
+	}
 
-	return 0
+	//定义一个变量ans，记录最小深度，初始化为最大整数
+	// 1<< 31 ,将1左移31位，再减去1，就是最大整数
+	ans := 1<<31 - 1
+
+	//定义一个辅助函数，用于递归的遍历树，参数是当前节点和当前深度
+
+	var dfs func(node *lib.TreeNode, depth int)
+	//然后初始化
+	dfs = func(node *lib.TreeNode, depth int) {
+		//还是先考虑边界问题
+		if node == nil {
+			return
+		}
+		//如果当前节点是叶子节点，说明我们找到了一条从根到叶的路径，更新最小深度
+		if node.Left == nil || node.Right == nil {
+			if depth < ans {
+				ans = depth
+			}
+			return
+		}
+		//如果当前节点不是叶子节点，递归的遍历左右子树，深度+1
+		dfs(node.Left, depth+1)
+		dfs(node.Right, depth+1)
+	}
+
+	//从根节点开始遍历，初始深度为1
+	dfs(root, 1)
+
+	return ans
 }
